9306c5d29473f5185b44ad445fb7dee3d2a8a7a9
larskulbe <lars.kulbe@gmx.de>
2025-01-22 17:33:12 +0100

    Add spiral_traversal function

---
+++ R/spiral_traversal.R
@@ -0,0 +1,46 @@
+spiral_traversal <- function(size) {
+  # Ensure that dimensions of matrix are odd
+  ifelse(size %% 2 == 0,
+         size <- size + 1,
+         size <- size)
+
+  # Get pixel_order of pixles from spiral traversal algorithm
+  mid <- median(1:size)
+  pixel_order <- matrix(mid, nrow = size^2, ncol = 2)
+  colnames(pixel_order) <- c("row", "col")
+  pointers <- c(mid - 1, mid - 1, mid + 1, mid + 1)
+  names(pointers) <- c("left", "top", "bottom", "right")
+  i <- 1
+
+  while (pointers["bottom"] <= size && pointers["right"] <= size) {
+    # left column, bottom -1 to top row
+    new_rows <- c((pointers["bottom"] - 1):pointers["top"])
+    new_cols <- rep_len(pointers["left"], length(new_rows))
+    pixel_order[c(i + seq_len(length(new_rows))), 1] <- new_rows
+    pixel_order[c(i + seq_len(length(new_cols))), 2] <- new_cols
+    i <- i + length(new_rows)
+    # top row, left +1 to right column
+    new_cols <- c((pointers["left"] + 1):pointers["right"])
+    new_rows <- rep_len(pointers["top"], length(new_cols))
+    pixel_order[c(i + seq_len(length(new_rows))), 1] <- new_rows
+    pixel_order[c(i + seq_len(length(new_cols))), 2] <- new_cols
+    i <- i + length(new_rows)
+    # right column, top +1 to bottom row
+    new_rows <- c((pointers["top"] + 1):pointers["bottom"])
+    new_cols <- rep_len(pointers["right"], length(new_rows))
+    pixel_order[c(i + seq_len(length(new_rows))), 1] <- new_rows
+    pixel_order[c(i + seq_len(length(new_cols))), 2] <- new_cols
+    i <- i + length(new_rows)
+    # bottom row, right -1 to left column
+    new_cols <- c((pointers["right"] - 1):pointers["left"])
+    new_rows <- rep_len(pointers["bottom"], length(new_cols))
+    pixel_order[c(i + seq_len(length(new_rows))), 1] <- new_rows
+    pixel_order[c(i + seq_len(length(new_cols))), 2] <- new_cols
+    i <- i + length(new_rows)
+
+    pointers[1:2] <- pointers[1:2] - 1
+    pointers[3:4] <- pointers[3:4] + 1
+  }
+  pixel_order <- list(order = pixel_order, n_rows = size, n_cols = size)
+  return(pixel_order)
+}
